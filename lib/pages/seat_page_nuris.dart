4c8fb996ffa36a48cb0ebc44a6c3a4186e0f2627
Gustianu <rafialfi20@gmail.com>
2025-11-30 20:37:50 +0700

    menambahkan grid kursi serta fitur berubah warna ketika diteka dan menambahkan gambar terbaru

--- lib/pages/seat_page_nuris.dart
+++ lib/pages/seat_page_nuris.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SeatPage_Nuris extends StatelessWidget {
   // final QueryDocumentSnapshot movie;
@@ -27,16 +28,17 @@ class SeatPage_Nuris extends StatelessWidget {
           Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              legendItem('images/kursi_abu_uas.png', "Kosong"),
-              legendItem('images/kursi_biru_uas.png', "Dipilih"),
-              legendItem('images/kursi_merah_uas.png', "Terjual"),
+              legendItems_Nuris('images/kursi_abu_uas_2.png', "Kosong"),
+              legendItems_Nuris('images/kursi_biru_uas_2.png', "Dipilih"),
+              legendItems_Nuris('images/kursi_merah_uas_2.png', "Terjual"),
             ],
           ),
           Divider(
             thickness: 1,        
             color: Colors.grey[300],  
           ),
-          SeatItem_Nuris()
+          Expanded(child: SeatItem_Nuris(),)
+          
         ],
       )
 
@@ -48,19 +50,75 @@ class SeatItem_Nuris extends StatefulWidget {
   const SeatItem_Nuris({super.key});
 
   @override
-  State<SeatItem_Nuris> createState() => _SeatItem_Nuris_state();
+  State<SeatItem_Nuris> createState() => _SeatItem_state_Nuris();
 }
 
-class _SeatItem_Nuris_state extends State<SeatItem_Nuris>{
+class _SeatItem_state_Nuris extends State<SeatItem_Nuris>{
+
+ final List<int> kursiPilihan = [];
   @override
   Widget build(BuildContext context) {
-    return  Center(
-        child: Text("Seat Page (placeholder)"),
-      );
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection("bookings").snapshots(), 
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return CircularProgressIndicator();
+
+        final docs = snapshot.data!.docs;
+        // final mediaQuery = MediaQuery.of(context);
+        // double tinggiLayar = mediaQuery.size.height;
+        // double lebarLayar = mediaQuery.size.width;
+
+        // final List<String> listKursi = List.generate((tinggiLayar*lebarLayar/(50*50)).toInt(), (i) => 'images/kursi_abu_uas.png');
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 12,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5
+          ),
+
+          itemCount: 150,
+          itemBuilder: (context, index) {
+            bool isPressed = kursiPilihan.contains(index);
+            final String imagePath = isPressed
+              ?'images/kursi_biru_uas_2.png'
+              :'images/kursi_abu_uas_2.png';
+                  // final m = docs[index];
+            return GestureDetector(
+              onTap: (){
+                setState(() {
+                  if (isPressed) {
+                    kursiPilihan.remove(index);
+                  } 
+                  else {
+                    kursiPilihan.add(index);
+                  }
+                });
+              },
+
+              child:Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    imagePath,
+                  ),
+                  Text(teksKursi_Nuris(index),
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              )
+            );
+          },
+        );
+      }
+    );
   }
 }
 
-Widget legendItem(String path, String label) {
+Widget legendItems_Nuris(String path, String label) {
   return Row(
     children: [
       Image.asset(path,
@@ -70,4 +128,12 @@ Widget legendItem(String path, String label) {
       Text(label),
     ],
   );
+}
+
+
+String teksKursi_Nuris(int index){
+  List<String> abjad = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K','L','M', 'N', 'O', 'P'];
+  int hitung = index ~/ 12;
+  int angka = (index%12)+1;
+  return "${abjad[hitung]}$angka";
 }
