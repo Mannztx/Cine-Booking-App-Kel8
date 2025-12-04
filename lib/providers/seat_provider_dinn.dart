2310b208ccdd5fd248b855dd3fb7513df6b5d016
Mannztx <ardiansyahfirman136@gmail.com>
2025-12-04 21:26:17 +0700

    perbaiki conflict/error

--- lib/providers/seat_provider_dinn.dart
+++ lib/providers/seat_provider_dinn.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cine_booking_app_kel8/controllers/booking_logic_controller.dart';
+import 'package:cine_booking_app_kel8/controllers/booking_logic_controller_dinn.dart';
 
-class SeatProvider extends ChangeNotifier {
+class SeatProvider_dinn extends ChangeNotifier {
   List<String> selectedSeats = [];
 
-  void toggleSeat(String seatCode) {
+  void toggleSeat_dinn(String seatCode) {
     if (selectedSeats.contains(seatCode)) {
       selectedSeats.remove(seatCode);
     } else {
@@ -21,26 +21,30 @@ class SeatProvider extends ChangeNotifier {
   }
 
   // METHOD YANG WAJIB ADA (INI YANG BIKIN CHECKOUT BERJALAN)
-  Future<void> checkoutToFirebase({
+  Future<void> checkoutToFirebase_dinn({
     required BuildContext context,
     required Map<String, dynamic> movie,
     required String userId,
   }) async {
     final uid = FirebaseAuth.instance.currentUser!.uid;
-    final logic = BookingLogicController();
-    double totalPrice = logic.calculateTotal(
-      movieTitle: movie["title"],
-      seats: selectedSeats,
-      basePrice: movie["base_price"].toDouble(),
+    final logic = BookingLogicController_dinn();
+    double totalPrice = logic.calculateTotal_dinn(
+      movieTitle_dinn: movie["title"],
+      seats_dinn: selectedSeats,
+      basePrice_dinn: movie["base_price"].toDouble(),
     );
 
     try {
-      await FirebaseFirestore.instance.collection("bookings").add({
+      final docRef = await FirebaseFirestore.instance.collection("bookings").add({
         "movie_title": movie["title"],
         "total_price": totalPrice.toInt(),
         "seats": selectedSeats,
         "booking_date": DateTime.now(),
         "user_id": uid,
+      });
+      final bookingId = docRef.id;
+      await docRef.update({
+        "booking_id": bookingId,
       });
 
       clearSeats(); // bersihkan pilihan
